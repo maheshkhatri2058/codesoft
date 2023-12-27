fd768b09f8bb255293d456fb03afbc686c20a113
Mahesh Khatri <106811847+maheshkhatri2058@users.noreply.github.com>
2023-12-27 18:48:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function shown()
+{
+    let menus=document.getElementById('right1');
+    menus.style.display='flex';
+    let menu=document.getElementById('mn');
+    menu.style.display='none';
+}
